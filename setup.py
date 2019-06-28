853fd5966e5f9c0a90c753c599412551f5722fd7
Natalie Widmann <NatalieWidmann@gmx.de>
2019-06-28 12:13:54 +0200

    remove failing fasttext requirement / add readme as description

--- setup.py
+++ setup.py
@@ -4,10 +4,14 @@ setup(
         name="semantic_search",
         version="0.1.0",
 
+        url="https://github.com/Tilana/semantic-search",
+        description="search for semantically related sentences",
+        long_description=open("README.md").read(),
+
+        packages=["semantic_search"],
         include_package_data=True,
 
         install_requires=[
-            "fasttext==0.8.22",
             "mypy==0.641",
             "nltk==3.4",
             "mypy-extensions==0.4.1",
